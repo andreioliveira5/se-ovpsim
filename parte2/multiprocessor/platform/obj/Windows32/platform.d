f994949d4e117603e89ec74928a6eea1657e8855
Andrei de Oliveira <166375@upf.br>
2020-11-23 20:59:27 -0300

    Adicionado as partes 2 e 3 iniciais

---
+++ parte2/multiprocessor/platform/obj/Windows32/platform.d
@@ -0,0 +1,7 @@
+obj/Windows32/platform.o: platform.c \
+ C:/Imperas/ImpPublic/include/host/icm/icmCpuManager.h \
+ C:/Imperas/ImpPublic/include/host/hostapi/impTypes.h \
+ C:/Imperas/ImpPublic/include/host/hostapi/impVersion.h \
+ C:/Imperas/ImpPublic/include/host/hostapi/hostMacros.h \
+ C:/Imperas/ImpPublic/include/host/ord/ordObjAttrs.h \
+ C:/Imperas/ImpPublic/include/host/ord/ordTypes.h
